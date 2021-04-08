6d534569df4391c31fcbfca77ba70f862cfeeb63
Kaique Silva <50109765+DevKaiqueSilva@users.noreply.github.com>
2021-04-07 21:11:56 -0300

    Add files via upload

--- Atividade7/main.js
+++ Atividade7/main.js
@@ -1,7 +1,22 @@
-let num1, num2, num3;
-num1 = prompt("Informe o primeiro número: ");
-num2 = prompt("Informe o segundo número: ");
-num3 = prompt("Informe o terceiro número: ");
+let num1 = '', num2 = '', num3 = '';
+do {
+    num1 = prompt("Informe o primeiro número: ");
+    if (isNaN(num1)) {
+        alert("Informe um número válido.");
+    }
+} while (isNaN(num1) == true)
+do {
+    num2 = prompt("Informe o segundo número: ");
+    if (isNaN(num2)) {
+        alert("Informe um número válido.");
+    }
+} while (isNaN(num2) == true)
+do {
+    num3 = prompt("Informe o terceiro número: ");
+    if (isNaN(num3)) {
+        alert("Informe um número válido.");
+    }
+} while (isNaN(num3) == true)
 
 const maiorNumero = (n1, n2, n3) => {
     let numbers = [n1, n2, n3];
@@ -11,6 +26,6 @@ const maiorNumero = (n1, n2, n3) => {
     return numbers[0];
 }
 
-alert(maiorNumero(num1, num2, num3));
+alert('O maior número é : ' + maiorNumero(num1, num2, num3));
 
 
